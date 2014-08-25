653734ccf5078999329c393c1daf41c14c1c827e
Justin Hughart <justinhughart@gmail.com>
2014-08-25 03:37:27 -0700

    -Created SOLMap.

---
+++ SpearOfLonginus/Maps/SOLMap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace SpearOfLonginus.Maps
+{
+    public class SOLMap
+    {
+        protected List<SOLTile> TileSet;
+
+        public SOLVector Size { get; protected set; }
+        public SOLVector TileSize { get; protected set; }
+
+        protected List<int> CollisionLayer;
+        protected List<int> Background;
+        protected List<int> Foreground;
+
+        protected List<MapLogic> Logics;
+
+
+
+
+
+        public void Update()
+        {
+            foreach (var tile in TileSet)
+            {
+                tile.Update();
+            }
+
+            foreach (var logic in Logics)
+            {
+                logic.Update();
+            }
+        }
+    }
+}
