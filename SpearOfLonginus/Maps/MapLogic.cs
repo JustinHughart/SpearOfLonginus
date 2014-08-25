380660e1072ef936cefc2c61781a5ee3fa0298b2
Justin Hughart <justinhughart@gmail.com>
2014-08-25 03:37:12 -0700

    -Created MapLogic

---
+++ SpearOfLonginus/Maps/MapLogic.cs
@@ -0,0 +1,27 @@
+﻿namespace SpearOfLonginus.Maps
+{
+    /// <summary>
+    /// An abstract class for level logic.
+    /// </summary>
+    public abstract class MapLogic
+    {
+        /// <summary>
+        /// The owner of the logic component.
+        /// </summary>
+        protected SOLMap Owner;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MapLogic"/> class.
+        /// </summary>
+        /// <param name="owner">The owner of the logic component.</param>
+        public MapLogic(SOLMap owner)
+        {
+            Owner = owner;
+        }
+
+        /// <summary>
+        /// Updates the logic component.
+        /// </summary>
+        public abstract void Update();
+    }
+}
