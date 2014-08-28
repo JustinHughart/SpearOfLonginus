e2a8eb3bb020406e9b7fcc556be8f69dfac58806
Justin Hughart <justinhughart@gmail.com>
2014-08-28 04:14:02 -0700

    -Updated SOLTile with its data, functions, and documentation.

--- SpearOfLonginus/Maps/SOLTile.cs
+++ SpearOfLonginus/Maps/SOLTile.cs
@@ -1,13 +1,48 @@
-﻿namespace SpearOfLonginus.Maps
+﻿using SpearOfLonginus.Animation;
+
+namespace SpearOfLonginus.Maps
 {
+    /// <summary>
+    /// A tile for a tile map in Spear of Longinus.
+    /// </summary>
     public class SOLTile
     {
-        public string TextureID { get; protected set; }
-        public bool Impassable { get; protected set; }
+        #region Variables
 
-        public virtual void Update()
+        /// <summary>
+        /// The tile's animation.
+        /// </summary>
+        /// <value>
+        /// The tile's animation.
+        /// </value>
+        public SOLAnimation Animation { get; protected set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SOLTile"/> class.
+        /// </summary>
+        /// <param name="animation">The tile's animation.</param>
+        public SOLTile(SOLAnimation animation)
         {
-
+            Animation = animation;
         }
+
+        #endregion 
+
+        #region Functions
+
+        /// <summary>
+        /// Updates the tile.
+        /// </summary>
+        /// <param name="animspeed">The speed at which to update the animation of the tile.</param>
+        public virtual void Update(float animspeed)
+        {
+            Animation.Update(animspeed);
+        }
+
+        #endregion
     }
 }
