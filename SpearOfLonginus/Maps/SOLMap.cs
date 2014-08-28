04f489119ca52a0cbb0139d654f65c0654b31e51
Justin Hughart <justinhughart@gmail.com>
2014-08-28 05:04:29 -0700

    -Added tileset loading and the start of map loading.

--- SpearOfLonginus/Maps/SOLMap.cs
+++ SpearOfLonginus/Maps/SOLMap.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Xml.Linq;
+using SpearOfLonginus.Animation;
 
 namespace SpearOfLonginus.Maps
 {
@@ -6,8 +11,8 @@ namespace SpearOfLonginus.Maps
     {
         protected List<SOLTile> TileSet;
 
-        public SOLVector Size { get; protected set; }
-        public SOLVector TileSize { get; protected set; }
+        public SOLVector Size;
+        public SOLVector TileSize;
 
         protected List<int> CollisionLayer;
         protected List<int> Background;
@@ -16,14 +21,18 @@ namespace SpearOfLonginus.Maps
         protected List<SOLMapLogic> Logics;
 
         protected List<SOLBackdrop> Backdrops;
-        protected List<SOLBackdrop> Foredrops; 
-        
+        protected List<SOLBackdrop> Foredrops;
 
-        public virtual void Update()
+        public SOLMap(string path)
+        {
+            LoadMap(path);
+        }
+
+        public virtual void Update(float animspeed)
         {
             foreach (var tile in TileSet)
             {
-                tile.Update();
+                tile.Update(animspeed);
             }
 
             foreach (var logic in Logics)
@@ -40,6 +49,242 @@ namespace SpearOfLonginus.Maps
             {
                 foredrop.Update();
             }
+        }
+
+        protected virtual void LoadMap(string path)
+        {
+            if (File.Exists(path)) //Check if file exists.
+            {
+                XDocument doc = XDocument.Load(path); //Load the file.
+                LoadMap(doc.Root);
+            }
+            else
+            {
+                throw new Exception("Failed to find map.");
+            }
+        }
+
+        protected virtual void LoadMap(XElement root)
+        {
+            if (root == null) //First check if the document actually has a root.
+            {
+                throw new Exception("Document has no root.");
+            }
+
+            if (root.Name != "map") //Then check if it's a tiled map.
+            {
+                throw new Exception("Document is not a Tiled map.");
+            }
+
+            //With that out of the way, we will check the map attributes.
+            foreach (var attribute in root.Attributes())
+            {
+                if (attribute.Name == "width")
+                {
+                    float.TryParse(attribute.Value, out Size.X);
+                    continue;
+                }
+
+                if (attribute.Name == "height")
+                {
+                    float.TryParse(attribute.Value, out Size.Y);
+                    continue;
+                }
+
+                if (attribute.Name == "tilewidth")
+                {
+                    float.TryParse(attribute.Value, out TileSize.X);
+                    continue;
+                }
+
+                if (attribute.Name == "tileheight")
+                {
+                    float.TryParse(attribute.Value, out TileSize.Y);
+                }
+            }
+
+            //Load map properties here? We don't have any by default. 
+
+            //Here we will load the external tilesets. External tilesets are extremely recommended (required by vanilla SOL actually) due to keeping everything uniform amongst your maps.
+
+            TileSet = new List<SOLTile>();
+            TileSet.Add(null); //Add a null value to equate GID 0 (no tile) to it.
+
+            foreach (var element in root.Elements("tileset"))
+            {
+                var source = element.Attribute("source");
+
+                if (source == null) //If there's no source value, this means the tileset is internal. This is terrible for RPGs, as you want uniformity between your reused tilesets.
+                {
+                    throw new Exception("Tileset is not external. Please make your tileset external.");
+                }
+
+                LoadTileset(source.Value);
+            }
+
+            //Load tile layers.
+
+            //Load object layers.
+        }
+
+        protected virtual void LoadTileset(string path)
+        {
+            if (File.Exists(path)) //Check if file exists.
+            {
+                XDocument doc = XDocument.Load(path); //Load the file.
+                LoadTileset(doc.Root, TileSize);
+            }
+            else
+            {
+                throw new Exception("Failed to find external tileset.");
+            }
+        }
+
+        protected virtual void LoadTileset(XElement root, SOLVector tilesize)
+        {
+            if (root == null) //First check if the document actually has a root.
+            {
+                throw new Exception("Document has no root.");
+            }
+
+            if (root.Name != "tileset") //Then check if it's an external tileset.
+            {
+                throw new Exception("Document is not an external tileset.");
+            }
+
+            //With that out of the way, we will check the image attributes.
+            string textureid = "";
+            SOLVector imagesize = SOLVector.Zero;
+
+            var imageelement = root.Element("image");
+
+            if (imageelement == null) //Gotta check if that's null.
+            {
+                throw new Exception("No texture element in proper location in document.");
+            }
+
+            //Check the attributes. We are ignoring trans since you should know how to handle your transparency in your given framework.
+
+            foreach (var attribute in imageelement.Attributes())
+            {
+                if (attribute.Name == "source")
+                {
+                    textureid = attribute.Value;
+                    continue;
+                }
+
+                if (attribute.Name == "width")
+                {
+                    float.TryParse(attribute.Value, out imagesize.X);
+                    continue;
+                }
+
+                if (attribute.Name == "height")
+                {
+                    float.TryParse(attribute.Value, out imagesize.Y);
+                }
+            }
+
+            //Divide the image size by tilesize to get the size in tiles. TileSize is already loaded from LoadMap, so it'll work fine. DOES NOT SUPPORT MALFORMED IMAGES, such as 37 x 43. All images must be a multiple of TileSize.
+            imagesize /= tilesize;
+
+            //Next, we will get sort the tiles with special properties into a dictionary for easy access.
+            var tileproperties = new Dictionary<int, XElement>();
+
+            foreach (var element in root.Elements("tile"))
+            {
+                var id = element.Attribute("id");
+
+                if (id == null)
+                {
+                    continue;
+                }
+
+                tileproperties.Add(int.Parse(id.Value), element);
+            }
+
+            //Load the tiles into the tileset.
+
+            int gid = 0;
+
+            for (int y = 0; y < imagesize.Y; y++)
+            {
+                for (int x = 0; y < imagesize.X; x++)
+                {
+                    //Get the element if it exists.
+                    XElement element = null;
+
+                    if (tileproperties.ContainsKey(gid))
+                    {
+                        element = tileproperties[gid];
+                    }
+
+                    //LOAD IT
+                    TileSet.Add(LoadTile(textureid, x, y, tilesize, element));
+
+                    //Increment the GID, for identification purposes.
+                    gid++;
+                }
+            }
+            
+            //And we're done, finally!
+        }
+
+        protected virtual SOLTile LoadTile(string textureid, SOLVector position, SOLVector tilesize, XElement element)
+        {
+            return LoadTile(textureid, (int) position.X, (int) position.Y, tilesize, element);
+        }
+
+        protected virtual SOLTile LoadTile(string textureid, int x, int y, SOLVector tilesize, XElement element)
+        {
+            int numframes = 1;
+            float animrate = 1; 
+
+            if (element != null) //If there's a data element with the tile...
+            {
+                var properties = element.Element("properties");
+                if (properties != null) //Ensure it actually has properties first.
+                {
+                    foreach (var property in properties.Elements("property")) //Check the properties.
+                    {
+                        var name = property.Attribute("name");
+                        var value = property.Attribute("value");
+                        
+                        if (name != null && value != null)
+                        {
+                            if (name.Value == "animframes") //The number of frames the animation has, stacked vertically directly below.
+                            {
+                                int.TryParse(value.Value, out numframes);
+                                continue;
+                            }
+
+                            if (name.Value == "animrate") //The rate at which the animation goes.
+                            {
+                                float.TryParse(value.Value, out animrate);
+                            }
+                        }
+                    }
+                }
+            }
+
+            //Get the list of frames.
+            var frames = new List<SOLFrame>();
+
+            for (int i = 0; i < numframes; i++)
+            {
+                GetTileFrame(textureid, x, y, tilesize, animrate);
+
+                y++;
+            }
+
+            return new SOLTile(new SOLAnimation(true, false, frames));
+        }
+
+        protected virtual SOLFrame GetTileFrame(string textureid, int x, int y, SOLVector tilesize, float animrate)
+        {
+            Rectangle drawrect = new Rectangle(x * (int)tilesize.X, y * (int)tilesize.Y, (int)tilesize.X, (int)tilesize.Y);
+            
+            return new SOLFrame(textureid, drawrect, SOLVector.Zero, animrate);
         }
     }
 }
