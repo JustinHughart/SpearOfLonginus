73ed9dd09861ff05d8dfeaec1544b5ca771aab79
Justin Hughart <justinhughart@gmail.com>
2014-08-28 01:05:57 -0700

    -Updated SOLFrame with its relevant information and proper documentation.

--- SpearOfLonginus/Animation/SOLFrame.cs
+++ SpearOfLonginus/Animation/SOLFrame.cs
@@ -1,19 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Drawing;
 
 namespace SpearOfLonginus.Animation
 {
+    /// <summary>
+    /// An animation frame for Spear of Longinus.
+    /// </summary>
     public class SOLFrame
     {
+        #region Variables
 
+        /// <summary>
+        /// The ID used for  texture loading.
+        /// </summary>
+        /// <value>
+        /// The ID.
+        /// </value>
+        public string TextureID { get; protected set; }
+        /// <summary>
+        /// The area on the texture that should be drawn.
+        /// </summary>
+        /// <value>
+        /// The draw area.
+        /// </value>
+        public Rectangle DrawArea { get; protected set; }
+        /// <summary>
+        /// The origin of the frame. Used for rotation and to offset the sprite.
+        /// </summary>
+        /// <value>
+        /// The origin.
+        /// </value>
+        public SOLVector Origin { get; protected set; }
+        /// <summary>
+        /// Gets or sets the time till next.
+        /// </summary>
+        /// <value>
+        /// The time until the frame changes.
+        /// </value>
+        public int TimeTillNext { get; protected set; }
 
+        #endregion
 
+        #region Constructors
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SOLFrame" /> class.
+        /// </summary>
+        /// <param name="textureid">The ID used for  texture loading.</param>
+        /// <param name="drawarea">The area on the texture that should be drawn.</param>
+        /// <param name="origin">The origin of the frame. Used for rotation and to offset the sprite.</param>
+        /// <param name="timetillnext">The time until the frame changes.</param>
+        public SOLFrame(string textureid, Rectangle drawarea, SOLVector origin, int timetillnext)
+        {
+            TextureID = textureid;
+            DrawArea = drawarea;
+            Origin = origin;
+            TimeTillNext = timetillnext;
+        }
 
-
-
-
+        #endregion
     }
 }
