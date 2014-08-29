163d2e0ecd76d750c08cff5766c79cfd875dfc1c
Justin <justinhughart@gmail.com>
2014-08-29 12:24:24 -0500

    -Split AnimationManager into two parts, one for SOL and its XNA binding.

--- SOLXNA/Animation/AnimationManager.cs
+++ SOLXNA/Animation/AnimationManager.cs
@@ -1,80 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using SpearOfLonginus.Animation;
 
 namespace SOLXNA.Animation
 {
     /// <summary>
     /// A class that holds animations.
     /// </summary>
-    public class AnimationManager
+    public class AnimationManager : SOLAnimationManager 
     {
-        #region Variables
-
-        /// <summary>
-        /// The keyed list of animations.
-        /// </summary>
-        protected Dictionary<string, Animation> Animations;
-
-        #endregion
-
-        #region Constructors
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AnimationManager" /> class.
-        /// </summary>
-        public AnimationManager()
-        {
-            Animations = new Dictionary<string, Animation>();
-        }
-
-        #endregion
-
         #region Functions
 
         /// <summary>
         /// Loads the textures
         /// </summary>
         /// <param name="texturemanager">The texture manager used for loading textures.</param>
-        public void LoadContent(TextureManager texturemanager)
+        public virtual void LoadContent(TextureManager texturemanager)
         {
-            foreach (var animation in Animations)
+            foreach (var animation  in Animations)
             {
-                animation.Value.LoadContent(texturemanager);
+                var anim = animation.Value as Animation;
+
+                anim.LoadContent(texturemanager);
             }
         }
 
         /// <summary>
         /// Unloads the textures.
         /// </summary>
-        public void UnloadContent()
+        public virtual void UnloadContent()
         {
             foreach (var animation in Animations)
             {
-                animation.Value.UnloadContent();
+                var anim = (Animation)animation.Value;
+
+                anim.UnloadContent();
             }
         }
 
-        /// <summary>
-        /// Adds the animation to the list of animations.
-        /// </summary>
-        /// <param name="key">The animation's key.</param>
-        /// <param name="animation">The animation to add.</param>
-        public virtual void AddAnimation(string key, Animation animation)
-        {
-            Animations.Add(key, animation);
-        }
-
-        /// <summary>
-        /// Retrieves the animation from the list of animations. Throws an exception if it doesn't exist.
-        /// </summary>
-        /// <param name="key">The key of the animation you're requesting..</param>
-        /// <returns></returns>
-        /// <exception cref="System.Exception">Animation "key" does not exist.</exception>
-        public virtual Animation GetAnimation(string key)
+        public virtual Animation GetXNAAnimation(string key)
         {
             if (!Animations.ContainsKey(key))
             {
-                return Animations[key].CloneAsXNAAnimation();
+                var anim = (Animation)Animations[key];
+
+                return anim.CloneAsXNAAnimation();
             }
 
             throw new Exception("Animation \"" + key+ "\" does not exist.");
---
+++ SpearOfLonginus/Animation/SOLAnimationManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpearOfLonginus.Animation
+{
+    /// <summary>
+    /// A class that holds animations.
+    /// </summary>
+    public class SOLAnimationManager
+    {
+        #region Variables
+
+        /// <summary>
+        /// The keyed list of animations.
+        /// </summary>
+        protected Dictionary<string, SOLAnimation> Animations;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SOLAnimationManager" /> class.
+        /// </summary>
+        public SOLAnimationManager()
+        {
+            Animations = new Dictionary<string, SOLAnimation>();
+        }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Adds the animation to the list of animations.
+        /// </summary>
+        /// <param name="key">The animation's key.</param>
+        /// <param name="animation">The animation to add.</param>
+        public virtual void AddAnimation(string key, SOLAnimation animation)
+        {
+            Animations.Add(key, animation);
+        }
+
+        /// <summary>
+        /// Retrieves the animation from the list of animations. Throws an exception if it doesn't exist.
+        /// </summary>
+        /// <param name="key">The key of the animation you're requesting..</param>
+        /// <returns></returns>
+        /// <exception cref="System.Exception">Animation "key" does not exist.</exception>
+        public virtual SOLAnimation GetAnimation(string key)
+        {
+            if (!Animations.ContainsKey(key))
+            {
+                return Animations[key].Clone();
+            }
+
+            throw new Exception("Animation \"" + key + "\" does not exist.");
+        }
+
+        #endregion
+    }
+}
