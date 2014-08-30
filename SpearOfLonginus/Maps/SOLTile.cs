9778038590afcdce2c9428f718d12a488bd3e2f5
Justin Hughart <justinhughart@gmail.com>
2014-08-30 04:33:03 -0700

    -Moved tile loading methods to SOLTile.

--- SpearOfLonginus/Maps/SOLTile.cs
+++ SpearOfLonginus/Maps/SOLTile.cs
@@ -1,4 +1,7 @@
-﻿using SpearOfLonginus.Animation;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Xml.Linq;
+using SpearOfLonginus.Animation;
 
 namespace SpearOfLonginus.Maps
 {
@@ -22,12 +25,37 @@ namespace SpearOfLonginus.Maps
         #region Constructors
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SOLTile"/> class.
+        /// Initializes a new instance of the <see cref="SOLTile" /> class.
         /// </summary>
         /// <param name="animation">The tile's animation.</param>
         public SOLTile(SOLAnimation animation)
         {
             Animation = animation;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SOLTile"/> class.
+        /// </summary>
+        /// <param name="textureid">The texture's ID.</param>
+        /// <param name="position">The position on the tilesheet.</param>
+        /// <param name="tilesize">The size of tiles.</param>
+        /// <param name="element">The XML element.</param>
+        public SOLTile(string textureid, SOLVector position, SOLVector tilesize, XElement element)
+        {
+            LoadTile(textureid, position, tilesize, element);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SOLTile"/> class.
+        /// </summary>
+        /// <param name="textureid">The texture's ID.</param>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="tilesize">The size of tiles.</param>
+        /// <param name="element">The XML element.</param>
+        public SOLTile(string textureid, int x, int y, SOLVector tilesize, XElement element)
+        {
+            LoadTile(textureid, x, y, tilesize, element);
         }
 
         #endregion 
@@ -41,6 +69,89 @@ namespace SpearOfLonginus.Maps
         public virtual void Update(float animspeed)
         {
             Animation.Update(animspeed);
+        }
+
+        /// <summary>
+        /// Loads the tile.
+        /// </summary>
+        /// <param name="textureid">The texture's ID.</param>
+        /// <param name="position">The position on the tilesheet.</param>
+        /// <param name="tilesize">The size of tiles.</param>
+        /// <param name="element">The XML element.</param>
+        /// <returns></returns>
+        protected virtual SOLTile LoadTile(string textureid, SOLVector position, SOLVector tilesize, XElement element)
+        {
+            return LoadTile(textureid, (int)position.X, (int)position.Y, tilesize, element);
+        }
+
+        /// <summary>
+        /// Loads the tile.
+        /// </summary>
+        /// <param name="textureid">The texture's ID.</param>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="tilesize">The size of tiles.</param>
+        /// <param name="element">The XML element.</param>
+        /// <returns></returns>
+        protected virtual SOLTile LoadTile(string textureid, int x, int y, SOLVector tilesize, XElement element)
+        {
+            int numframes = 1;
+            float animrate = 1;
+
+            if (element != null) //If there's a data element with the tile...
+            {
+                var properties = element.Element("properties");
+                if (properties != null) //Ensure it actually has properties first.
+                {
+                    foreach (var property in properties.Elements("property")) //Check the properties.
+                    {
+                        var name = property.Attribute("name");
+                        var value = property.Attribute("value");
+
+                        if (name != null && value != null)
+                        {
+                            if (name.Value == "animframes") //The number of frames the animation has, stacked vertically directly below.
+                            {
+                                int.TryParse(value.Value, out numframes);
+                                continue;
+                            }
+
+                            if (name.Value == "animrate") //The rate at which the animation goes.
+                            {
+                                float.TryParse(value.Value, out animrate);
+                            }
+                        }
+                    }
+                }
+            }
+
+            //Get the list of frames.
+            var frames = new List<SOLFrame>();
+
+            for (int i = 0; i < numframes; i++)
+            {
+                CreateTileFrame(textureid, x, y, tilesize, animrate);
+
+                y++;
+            }
+
+            return new SOLTile(new SOLAnimation(true, false, frames));
+        }
+
+        /// <summary>
+        /// Creates a new animation from for your tile.
+        /// </summary>
+        /// <param name="textureid">The texture's ID.</param>
+        /// <param name="x">The x.</param>
+        /// <param name="y">The y.</param>
+        /// <param name="tilesize">The size of tiles.</param>
+        /// <param name="element">The XML element.</param>
+        /// <returns></returns>
+        protected virtual SOLFrame CreateTileFrame(string textureid, int x, int y, SOLVector tilesize, float animrate)
+        {
+            Rectangle drawrect = new Rectangle(x * (int)tilesize.X, y * (int)tilesize.Y, (int)tilesize.X, (int)tilesize.Y);
+
+            return new SOLFrame(textureid, drawrect, SOLVector.Zero, animrate);
         }
 
         #endregion
