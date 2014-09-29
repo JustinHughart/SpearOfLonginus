bb81433bbcb0442f9bc7d2a92f405bad0927ac70
Justin Hughart <justinhughart@gmail.com>
2014-09-29 04:04:57 -0700

    -Made an InputManager abstract class.

---
+++ SpearOfLonginus/Input/InputManager.cs
@@ -0,0 +1,29 @@
+﻿namespace SpearOfLonginus.Input
+{
+    /// <summary>
+    /// An abstract class to define functions for a manager to create input packets from player input.
+    /// </summary>
+    public abstract class InputManager
+    {
+        /// <summary>
+        /// Gets the input packet for player one.
+        /// </summary>
+        /// <returns></returns>
+        public abstract InputPacket GetPlayer1Packet();
+        /// <summary>
+        /// Gets the input packet for player two.
+        /// </summary>
+        /// <returns></returns>
+        public abstract InputPacket GetPlayer2Packet();
+        /// <summary>
+        /// Gets the input packet for player three.
+        /// </summary>
+        /// <returns></returns>
+        public abstract InputPacket GetPlayer3Packet();
+        /// <summary>
+        /// Gets the input packet for player four.
+        /// </summary>
+        /// <returns></returns>
+        public abstract InputPacket GetPlayer4Packet();
+    }
+}
