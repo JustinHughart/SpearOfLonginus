357660f5a5a236070c301516ba26a5d202f6eea6
Justin Hughart <justinhughart@gmail.com>
2014-10-04 04:16:34 -0700

    -Started XnaInputManager and got it to a functioning state for 1P on keyboard.

---
+++ SOLXNA/Input/XnaInputManager.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework.Input;
+using SpearOfLonginus.Input;
+
+namespace SOLXNA.Input
+{
+    public class XnaInputManager : InputManager
+    {
+        public KeyboardProfile KBProfile;
+
+        protected KeyboardState CurrKBState;
+        protected KeyboardState PrevKBState;
+
+        public XnaInputManager()
+        {
+            KBProfile = new KeyboardProfile();
+
+            CurrKBState = PrevKBState = Keyboard.GetState();
+        }
+
+        public void Update()
+        {
+            PrevKBState = CurrKBState;
+            CurrKBState = Keyboard.GetState();
+        }
+
+        public override InputPacket GetPlayer1Packet()
+        {
+            return GetKeyboardPacket();
+        }
+
+        public override InputPacket GetPlayer2Packet()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override InputPacket GetPlayer3Packet()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override InputPacket GetPlayer4Packet()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private InputPacket GetKeyboardPacket()
+        {
+            return new InputPacket(
+                GetKeyState(KBProfile.Up),
+                GetKeyState(KBProfile.Down),
+                GetKeyState(KBProfile.Left),
+                GetKeyState(KBProfile.Right),
+                GetKeyState(KBProfile.Accept),
+                GetKeyState(KBProfile.Cancel),
+                GetKeyState(KBProfile.Run),
+                GetKeyState(KBProfile.Check)
+                );
+        }
+
+        private PressState GetKeyState(Keys key)
+        {
+            if (CurrKBState.IsKeyDown(key))
+            {
+                if (PrevKBState.IsKeyDown(key))
+                {
+                    return PressState.Down;
+                }
+                else
+                {
+                    return PressState.Pressed;
+                }
+            }
+            else
+            {
+                if (PrevKBState.IsKeyUp(key))
+                {
+                    return PressState.Up;
+                }
+                else
+                {
+                    return PressState.Released;
+                }
+            }
+        }
+    }
+}
