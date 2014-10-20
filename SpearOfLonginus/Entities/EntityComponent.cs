c471db3415dbd9eefc07324c8cfa4fd9e6ca4564
Justin <justinhughart@gmail.com>
2014-10-20 06:01:30 -0500

    -Added a base for entity components.

---
+++ SpearOfLonginus/Entities/EntityComponent.cs
@@ -0,0 +1,34 @@
+﻿using SpearOfLonginus.Input;
+
+namespace SpearOfLonginus.Entities
+{
+    /// <summary>
+    /// A logical component for the entity class.
+    /// </summary>
+    public abstract class EntityComponent
+    {
+        /// <summary>
+        /// The entity who owns the component.
+        /// </summary>
+        protected Entity Owner;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EntityComponent"/> class.
+        /// </summary>
+        /// <param name="owner">The entity who owns the component.</param>
+        public EntityComponent(Entity owner)
+        {
+            Owner = owner;
+        }
+
+        /// <summary>
+        /// Updates the specified packet.
+        /// </summary>
+        /// <param name="packet">The packet of input data.</param>
+        /// <param name="deltatime">The time that has passed since last update.</param>
+        public virtual void Update(InputPacket packet, float deltatime)
+        {
+            
+        }
+    }
+}
