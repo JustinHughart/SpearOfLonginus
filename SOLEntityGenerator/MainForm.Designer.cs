729a03aac7a0104194baad1b2efe6ad86ef2d5de
Justin Hughart <justinhughart@gmail.com>
2014-11-02 03:16:26 -0800

    -Updated Mainform with basic data.

--- SOLEntityGenerator/MainForm.Designer.cs
+++ SOLEntityGenerator/MainForm.Designer.cs
@@ -28,20 +28,369 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cboFacing = new System.Windows.Forms.ComboBox();
+            this.lblFacing = new System.Windows.Forms.Label();
+            this.cboFacingStyle = new System.Windows.Forms.ComboBox();
+            this.lblFacingStyle = new System.Windows.Forms.Label();
+            this.cboInputType = new System.Windows.Forms.ComboBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.menuFileMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.numHitboxX = new System.Windows.Forms.NumericUpDown();
+            this.numHitboxY = new System.Windows.Forms.NumericUpDown();
+            this.numHitboxW = new System.Windows.Forms.NumericUpDown();
+            this.numHitboxH = new System.Windows.Forms.NumericUpDown();
+            this.lblHitboxX = new System.Windows.Forms.Label();
+            this.lblHitboxY = new System.Windows.Forms.Label();
+            this.lblHitboxW = new System.Windows.Forms.Label();
+            this.lblHitboxH = new System.Windows.Forms.Label();
+            this.lblSolid = new System.Windows.Forms.Label();
+            this.chkSolid = new System.Windows.Forms.CheckBox();
+            this.templateEditorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.menuFileMenu.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numHitboxX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHitboxY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHitboxW)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHitboxH)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.cboFacing);
+            this.panel1.Controls.Add(this.lblFacing);
+            this.panel1.Controls.Add(this.cboFacingStyle);
+            this.panel1.Controls.Add(this.lblFacingStyle);
+            this.panel1.Controls.Add(this.cboInputType);
+            this.panel1.Controls.Add(this.lblInput);
+            this.panel1.Controls.Add(this.txtID);
+            this.panel1.Controls.Add(this.lblID);
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(258, 115);
+            this.panel1.TabIndex = 0;
+            // 
+            // cboFacing
+            // 
+            this.cboFacing.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFacing.FormattingEnabled = true;
+            this.cboFacing.Location = new System.Drawing.Point(79, 85);
+            this.cboFacing.Name = "cboFacing";
+            this.cboFacing.Size = new System.Drawing.Size(168, 21);
+            this.cboFacing.TabIndex = 7;
+            // 
+            // lblFacing
+            // 
+            this.lblFacing.AutoSize = true;
+            this.lblFacing.Location = new System.Drawing.Point(28, 88);
+            this.lblFacing.Name = "lblFacing";
+            this.lblFacing.Size = new System.Drawing.Size(45, 13);
+            this.lblFacing.TabIndex = 6;
+            this.lblFacing.Text = "Facing: ";
+            // 
+            // cboFacingStyle
+            // 
+            this.cboFacingStyle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFacingStyle.FormattingEnabled = true;
+            this.cboFacingStyle.Location = new System.Drawing.Point(79, 57);
+            this.cboFacingStyle.Name = "cboFacingStyle";
+            this.cboFacingStyle.Size = new System.Drawing.Size(168, 21);
+            this.cboFacingStyle.TabIndex = 5;
+            // 
+            // lblFacingStyle
+            // 
+            this.lblFacingStyle.AutoSize = true;
+            this.lblFacingStyle.Location = new System.Drawing.Point(2, 60);
+            this.lblFacingStyle.Name = "lblFacingStyle";
+            this.lblFacingStyle.Size = new System.Drawing.Size(71, 13);
+            this.lblFacingStyle.TabIndex = 4;
+            this.lblFacingStyle.Text = "Facing Style: ";
+            // 
+            // cboInputType
+            // 
+            this.cboInputType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboInputType.FormattingEnabled = true;
+            this.cboInputType.Location = new System.Drawing.Point(79, 30);
+            this.cboInputType.Name = "cboInputType";
+            this.cboInputType.Size = new System.Drawing.Size(168, 21);
+            this.cboInputType.TabIndex = 3;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(9, 33);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(64, 13);
+            this.lblInput.TabIndex = 2;
+            this.lblInput.Text = "Input Type: ";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(79, 4);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(168, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(49, 7);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(24, 13);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID: ";
+            // 
+            // menuFileMenu
+            // 
+            this.menuFileMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.templateEditorToolStripMenuItem});
+            this.menuFileMenu.Location = new System.Drawing.Point(0, 0);
+            this.menuFileMenu.Name = "menuFileMenu";
+            this.menuFileMenu.Size = new System.Drawing.Size(566, 24);
+            this.menuFileMenu.TabIndex = 1;
+            this.menuFileMenu.Text = "FileMenu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.NewToolStripMenuItemClick);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItemClick);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItemClick);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.SaveAsToolStripMenuItemClick);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.chkSolid);
+            this.panel3.Controls.Add(this.lblSolid);
+            this.panel3.Controls.Add(this.lblHitboxH);
+            this.panel3.Controls.Add(this.lblHitboxW);
+            this.panel3.Controls.Add(this.lblHitboxY);
+            this.panel3.Controls.Add(this.lblHitboxX);
+            this.panel3.Controls.Add(this.numHitboxH);
+            this.panel3.Controls.Add(this.numHitboxW);
+            this.panel3.Controls.Add(this.numHitboxY);
+            this.panel3.Controls.Add(this.numHitboxX);
+            this.panel3.Location = new System.Drawing.Point(12, 148);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(258, 130);
+            this.panel3.TabIndex = 2;
+            // 
+            // numHitboxX
+            // 
+            this.numHitboxX.Location = new System.Drawing.Point(79, 4);
+            this.numHitboxX.Name = "numHitboxX";
+            this.numHitboxX.Size = new System.Drawing.Size(168, 20);
+            this.numHitboxX.TabIndex = 0;
+            // 
+            // numHitboxY
+            // 
+            this.numHitboxY.Location = new System.Drawing.Point(79, 30);
+            this.numHitboxY.Name = "numHitboxY";
+            this.numHitboxY.Size = new System.Drawing.Size(168, 20);
+            this.numHitboxY.TabIndex = 1;
+            // 
+            // numHitboxW
+            // 
+            this.numHitboxW.Location = new System.Drawing.Point(79, 56);
+            this.numHitboxW.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numHitboxW.Name = "numHitboxW";
+            this.numHitboxW.Size = new System.Drawing.Size(168, 20);
+            this.numHitboxW.TabIndex = 2;
+            this.numHitboxW.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numHitboxH
+            // 
+            this.numHitboxH.Location = new System.Drawing.Point(79, 82);
+            this.numHitboxH.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numHitboxH.Name = "numHitboxH";
+            this.numHitboxH.Size = new System.Drawing.Size(168, 20);
+            this.numHitboxH.TabIndex = 3;
+            this.numHitboxH.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblHitboxX
+            // 
+            this.lblHitboxX.AutoSize = true;
+            this.lblHitboxX.Location = new System.Drawing.Point(20, 6);
+            this.lblHitboxX.Name = "lblHitboxX";
+            this.lblHitboxX.Size = new System.Drawing.Size(53, 13);
+            this.lblHitboxX.TabIndex = 8;
+            this.lblHitboxX.Text = "Hitbox X: ";
+            // 
+            // lblHitboxY
+            // 
+            this.lblHitboxY.AutoSize = true;
+            this.lblHitboxY.Location = new System.Drawing.Point(20, 32);
+            this.lblHitboxY.Name = "lblHitboxY";
+            this.lblHitboxY.Size = new System.Drawing.Size(53, 13);
+            this.lblHitboxY.TabIndex = 9;
+            this.lblHitboxY.Text = "Hitbox Y: ";
+            // 
+            // lblHitboxW
+            // 
+            this.lblHitboxW.AutoSize = true;
+            this.lblHitboxW.Location = new System.Drawing.Point(16, 58);
+            this.lblHitboxW.Name = "lblHitboxW";
+            this.lblHitboxW.Size = new System.Drawing.Size(57, 13);
+            this.lblHitboxW.TabIndex = 10;
+            this.lblHitboxW.Text = "Hitbox W: ";
+            // 
+            // lblHitboxH
+            // 
+            this.lblHitboxH.AutoSize = true;
+            this.lblHitboxH.Location = new System.Drawing.Point(19, 84);
+            this.lblHitboxH.Name = "lblHitboxH";
+            this.lblHitboxH.Size = new System.Drawing.Size(54, 13);
+            this.lblHitboxH.TabIndex = 11;
+            this.lblHitboxH.Text = "Hitbox H: ";
+            // 
+            // lblSolid
+            // 
+            this.lblSolid.AutoSize = true;
+            this.lblSolid.Location = new System.Drawing.Point(37, 109);
+            this.lblSolid.Name = "lblSolid";
+            this.lblSolid.Size = new System.Drawing.Size(36, 13);
+            this.lblSolid.TabIndex = 12;
+            this.lblSolid.Text = "Solid: ";
+            // 
+            // chkSolid
+            // 
+            this.chkSolid.AutoSize = true;
+            this.chkSolid.Location = new System.Drawing.Point(79, 109);
+            this.chkSolid.Name = "chkSolid";
+            this.chkSolid.Size = new System.Drawing.Size(15, 14);
+            this.chkSolid.TabIndex = 13;
+            this.chkSolid.UseVisualStyleBackColor = true;
+            // 
+            // templateEditorToolStripMenuItem
+            // 
+            this.templateEditorToolStripMenuItem.Name = "templateEditorToolStripMenuItem";
+            this.templateEditorToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
+            this.templateEditorToolStripMenuItem.Text = "Template Editor";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(566, 372);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuFileMenu);
+            this.MainMenuStrip = this.menuFileMenu;
             this.Name = "MainForm";
             this.Text = "Spear of Longinus Entity Generator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuFileMenu.ResumeLayout(false);
+            this.menuFileMenu.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numHitboxX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHitboxY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHitboxW)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHitboxH)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.ComboBox cboInputType;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.ComboBox cboFacingStyle;
+        private System.Windows.Forms.Label lblFacingStyle;
+        private System.Windows.Forms.ComboBox cboFacing;
+        private System.Windows.Forms.Label lblFacing;
+        private System.Windows.Forms.MenuStrip menuFileMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.CheckBox chkSolid;
+        private System.Windows.Forms.Label lblSolid;
+        private System.Windows.Forms.Label lblHitboxH;
+        private System.Windows.Forms.Label lblHitboxW;
+        private System.Windows.Forms.Label lblHitboxY;
+        private System.Windows.Forms.Label lblHitboxX;
+        private System.Windows.Forms.NumericUpDown numHitboxH;
+        private System.Windows.Forms.NumericUpDown numHitboxW;
+        private System.Windows.Forms.NumericUpDown numHitboxY;
+        private System.Windows.Forms.NumericUpDown numHitboxX;
+        private System.Windows.Forms.ToolStripMenuItem templateEditorToolStripMenuItem;
     }
 }
 
--- SOLEntityGenerator/MainForm.cs
+++ SOLEntityGenerator/MainForm.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using SpearOfLonginus.Entities;
 
 namespace SOLEntityGenerator
 {
@@ -14,6 +9,75 @@ namespace SOLEntityGenerator
         public MainForm()
         {
             InitializeComponent();
+
+            SetupNumberBoxes();
+            FillComboBoxes();
         }
+
+        private void SetupNumberBoxes()
+        {
+            numHitboxX.Minimum = decimal.MinValue;
+            numHitboxX.Maximum = decimal.MaxValue;
+            numHitboxY.Minimum = decimal.MinValue;
+            numHitboxY.Maximum = decimal.MaxValue;
+            numHitboxW.Maximum = decimal.MaxValue;
+            numHitboxH.Maximum = decimal.MaxValue;
+        }
+
+        private void FillComboBoxes()
+        {
+            //Input Type
+            cboInputType.Items.Add(InputType.NPC);
+            cboInputType.Items.Add(InputType.Player1);
+            cboInputType.Items.Add(InputType.Player2);
+            cboInputType.Items.Add(InputType.Player3);
+            cboInputType.Items.Add(InputType.Player4);
+            cboInputType.Items.Add(InputType.World);
+            cboInputType.SelectedIndex = 0;
+
+            //Facing Style
+            cboFacingStyle.Items.Add(FacingStyle.Static);
+            cboFacingStyle.Items.Add(FacingStyle.FourWay);
+            cboFacingStyle.Items.Add(FacingStyle.EightWay);
+            cboFacingStyle.SelectedIndex = 2;
+
+            //Facing
+            cboFacing.Items.Add(FacingState.North);
+            cboFacing.Items.Add(FacingState.South);
+            cboFacing.Items.Add(FacingState.East);
+            cboFacing.Items.Add(FacingState.West);
+            cboFacing.Items.Add(FacingState.Northwest);
+            cboFacing.Items.Add(FacingState.Northeast);
+            cboFacing.Items.Add(FacingState.Southwest);
+            cboFacing.Items.Add(FacingState.Southeast);
+            cboFacing.SelectedIndex = 1;
+        }
+
+        private void NewToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveAsToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            
+        }
+
+        
     }
 }
