aeb6a02ee12e194b9d520c163de475aa2a89b1a7
Justin <justinhughart@gmail.com>
2014-11-06 05:19:20 -0600

    -Added TextInputForm in its entirety.

---
+++ SOLEntityGenerator/Controls/TextInputForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SOLEntityGenerator.Controls
+{
+    partial class TextInputForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(64, 39);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.BtnOKClick);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(145, 39);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 13);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(260, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // TextInputForm
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(284, 70);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TextInputForm";
+            this.Text = "TextInputForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
---
+++ SOLEntityGenerator/Controls/TextInputForm.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Forms;
+
+namespace SOLEntityGenerator.Controls
+{
+    /// <summary>
+    /// A class for receiving input from the user.
+    /// </summary>
+    public partial class TextInputForm : Form
+    {
+        /// <summary>
+        /// The input received from the user.
+        /// </summary>
+        public string Input;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextInputForm"/> class.
+        /// </summary>
+        /// <param name="title">The title of the window.</param>
+        public TextInputForm(string title)
+        {
+            InitializeComponent();
+            Input = "";
+            txtInput.Focus();
+            Text = title;
+        }
+
+        /// <summary>
+        /// The click command for btnOK.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void BtnOKClick(object sender, System.EventArgs e)
+        {
+            Input = txtInput.Text;
+        }
+    }
+}
