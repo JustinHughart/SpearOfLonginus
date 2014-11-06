498af20188563357fa442a71ff4898d700c16c66
Justin <justinhughart@gmail.com>
2014-11-06 05:28:13 -0600

    -Added a functioning ComponentForm.

---
+++ SOLEntityGenerator/ComponentForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace SOLEntityGenerator
+{
+    partial class ComponentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToEntityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitWithoutSavingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.templateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadTemplateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToTemplateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.xmlEditor = new SOLEntityGenerator.Controls.XmlEditorControl();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 250);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add Node";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAddClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.templateToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(281, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToEntityToolStripMenuItem,
+            this.exitWithoutSavingToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToEntityToolStripMenuItem
+            // 
+            this.saveToEntityToolStripMenuItem.Name = "saveToEntityToolStripMenuItem";
+            this.saveToEntityToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.saveToEntityToolStripMenuItem.Text = "Save To Entity";
+            this.saveToEntityToolStripMenuItem.Click += new System.EventHandler(this.SaveToEntityToolStripMenuItemClick);
+            // 
+            // exitWithoutSavingToolStripMenuItem
+            // 
+            this.exitWithoutSavingToolStripMenuItem.Name = "exitWithoutSavingToolStripMenuItem";
+            this.exitWithoutSavingToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.exitWithoutSavingToolStripMenuItem.Text = "Exit Without Saving";
+            this.exitWithoutSavingToolStripMenuItem.Click += new System.EventHandler(this.ExitWithoutSavingToolStripMenuItemClick);
+            // 
+            // templateToolStripMenuItem
+            // 
+            this.templateToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadTemplateToolStripMenuItem,
+            this.saveToTemplateToolStripMenuItem});
+            this.templateToolStripMenuItem.Name = "templateToolStripMenuItem";
+            this.templateToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.templateToolStripMenuItem.Text = "Template";
+            // 
+            // loadTemplateToolStripMenuItem
+            // 
+            this.loadTemplateToolStripMenuItem.Name = "loadTemplateToolStripMenuItem";
+            this.loadTemplateToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.loadTemplateToolStripMenuItem.Text = "Load Template";
+            // 
+            // saveToTemplateToolStripMenuItem
+            // 
+            this.saveToTemplateToolStripMenuItem.Name = "saveToTemplateToolStripMenuItem";
+            this.saveToTemplateToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.saveToTemplateToolStripMenuItem.Text = "Save To Template";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(104, 250);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Edit Node";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.BtnEditClick);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(196, 250);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete Node";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDeleteClick);
+            // 
+            // xmlEditor
+            // 
+            this.xmlEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.xmlEditor.HideSelection = false;
+            this.xmlEditor.Location = new System.Drawing.Point(12, 27);
+            this.xmlEditor.Name = "xmlEditor";
+            this.xmlEditor.Size = new System.Drawing.Size(260, 217);
+            this.xmlEditor.TabIndex = 0;
+            // 
+            // ComponentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 282);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.xmlEditor);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ComponentForm";
+            this.Text = "ComponentForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controls.XmlEditorControl xmlEditor;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToEntityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitWithoutSavingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem templateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadTemplateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToTemplateToolStripMenuItem;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ SOLEntityGenerator/ComponentForm.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SOLEntityGenerator
+{
+    public partial class ComponentForm : Form
+    {
+        XElement _element;
+
+        public ComponentForm(XElement element)
+        {
+            InitializeComponent();
+            _element = element;
+            xmlEditor.LoadXml(_element);
+        }
+
+        private void BtnAddClick(object sender, System.EventArgs e)
+        {
+            xmlEditor.AddNewNode();
+        }
+
+        private void BtnEditClick(object sender, System.EventArgs e)
+        {
+            xmlEditor.EditCurrentNode();
+        }
+
+        private void BtnDeleteClick(object sender, System.EventArgs e)
+        {
+            xmlEditor.DeleteCurrentNode();
+        }
+
+        private void SaveToEntityToolStripMenuItemClick(object sender, System.EventArgs e)
+        {
+            var saveelement = xmlEditor.ConvertToXml();
+
+            _element.Name = saveelement.Name;
+            _element.RemoveAll();
+            _element.Add(saveelement.Attributes());
+            _element.Add(saveelement.Elements());
+
+            Close();
+        }
+
+        private void ExitWithoutSavingToolStripMenuItemClick(object sender, System.EventArgs e)
+        {
+            Close();
+        }
+    }
+}
