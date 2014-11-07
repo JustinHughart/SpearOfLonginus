fac9ee9993c2e82095159c99ccd6e869c1af62c9
Justin Hughart <justinhughart@gmail.com>
2014-11-07 02:39:14 -0800

    -Created IXmlLoadable.

---
+++ SpearOfLonginus/IXmlLoadable.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+
+namespace SpearOfLonginus
+{
+    /// <summary>
+    /// An interface that says you may load the object from XML.
+    /// </summary>
+    public interface IXmlLoadable
+    {
+        /// <summary>
+        /// Uses XML to initialize the object.
+        /// </summary>
+        /// <param name="element">The element used for loading..</param>
+        void LoadFromXml(XElement element);
+    }
+}
