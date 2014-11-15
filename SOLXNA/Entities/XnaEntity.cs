d06d58c4eaccfc2e7f0f0f8816fe04930c7b90e6
Justin Hughart <justinhughart@gmail.com>
2014-11-15 05:21:28 -0800

    -Documented XnaEntity since I apparantly forgot to earlier.

--- SOLXNA/Entities/XnaEntity.cs
+++ SOLXNA/Entities/XnaEntity.cs
@@ -7,36 +7,77 @@ using Rectangle = SpearOfLonginus.Rectangle;
 
 namespace SOLXNA.Entities
 {
+    /// <summary>
+    /// An XNA binding for SOL's Entity.
+    /// </summary>
     public class XnaEntity : Entity
     {
+        /// <summary>
+        /// The sprite batch data.
+        /// </summary>
         protected SpriteBatchData SpriteBatchData;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XnaEntity"/> class.
+        /// </summary>
+        /// <param name="animationcache">The cache holding all the entities animations.</param>
         public XnaEntity(AnimationCache animationcache) : this( animationcache, new Rectangle(0,0,1,1))
         {
 
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XnaEntity"/> class.
+        /// </summary>
+        /// <param name="animationcache">The cache holding all the entities animations.</param>
+        /// <param name="hitbox">The hitbox that causes interactions with the world.</param>
         public XnaEntity(AnimationCache animationcache, Rectangle hitbox) : this(animationcache, hitbox, new SpriteBatchData())
         {
             
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XnaEntity"/> class.
+        /// </summary>
+        /// <param name="animationcache">The animationcache.</param>
+        /// <param name="hitbox">The hitbox.</param>
+        /// <param name="spritebatchdata">The spritebatchdata.</param>
         public XnaEntity(AnimationCache animationcache, Rectangle hitbox, SpriteBatchData spritebatchdata)
             : base(animationcache, hitbox)
         {
             SpriteBatchData = spritebatchdata;
         }
 
+        /// <summary>
+        /// Loads the content.
+        /// </summary>
+        /// <param name="texturecache">The texture cache used to load the content.</param>
         public void LoadContent(TextureCache texturecache)
         {
             var xnaanims = AnimationCache as XnaAnimationCache;
 
-            if (xnaanims != null)
+            if (xnaanims == null)
             {
-                xnaanims.LoadContent(texturecache);
+                var oldcache = AnimationCache;
+
+                AnimationCache = new XnaAnimationCache();
+
+                foreach (var animation in oldcache.Animations.Values)
+                {
+                    AnimationCache.AddAnimation(animation);
+                }
+
+                xnaanims = AnimationCache as XnaAnimationCache;
             }
+
+            xnaanims.LoadContent(texturecache);
         }
 
+        /// <summary>
+        /// Draws the entity.
+        /// </summary>
+        /// <param name="spritebatch">The sprite batch.</param>
+        /// <param name="cameramatrix">The camera matrix.</param>
         public virtual void Draw(SpriteBatch spritebatch, Matrix cameramatrix)
         {
             var anim = CurrentAnimation as XnaAnimation;
@@ -51,6 +92,12 @@ namespace SOLXNA.Entities
             }
         }
 
+        /// <summary>
+        /// Draws the hitbox.
+        /// </summary>
+        /// <param name="spritebatch">The sprite batch.</param>
+        /// <param name="cameramatrix">The camera matrix.</param>
+        /// <param name="texture">The texture.</param>
         public virtual void DrawHitbox(SpriteBatch spritebatch, Matrix cameramatrix, Texture2D texture)
         {
             Color color = Color.Aqua * .7f;
@@ -60,9 +107,6 @@ namespace SOLXNA.Entities
             spritebatch.Draw(texture, Hitbox.Location.ToXnaVector(), texture.Bounds, color, 0f, Vector2.Zero, Hitbox.Size.ToXnaVector(), SpriteEffects.None, 0f);
 
             spritebatch.End();
-
-
-
         }
     }
 }
