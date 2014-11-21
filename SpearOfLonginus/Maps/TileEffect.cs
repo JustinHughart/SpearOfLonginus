cc880b184a6f7f80bbfda4fabfe4967b940dd572
Justin Hughart <justinhughart@gmail.com>
2014-11-21 02:54:48 -0800

    -Added TileEffect.

---
+++ SpearOfLonginus/Maps/TileEffect.cs
@@ -0,0 +1,34 @@
+﻿using SpearOfLonginus.Entities;
+
+namespace SpearOfLonginus.Maps
+{
+    /// <summary>
+    /// A tile's effect. 
+    /// </summary>
+    public class TileEffect
+    {
+        /// <summary>
+        /// The owner of the effect.
+        /// </summary>
+        protected Tile Owner;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TileEffect"/> class.
+        /// </summary>
+        /// <param name="owner">The owner of the effect.</param>
+        public TileEffect(Tile owner)
+        {
+            Owner = owner;
+        }
+
+        /// <summary>
+        /// Called when the effect is activated.
+        /// </summary>
+        /// <param name="position">The position of the tile that was activated.</param>
+        /// <param name="entity">The entity that triggered the activation.</param>
+        public virtual void OnActivate(Vector position, Entity entity)
+        {
+
+        }
+    }
+}
