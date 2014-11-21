037476af56d0764bb278c3bb192c7a468927f586
Justin <justinhughart@gmail.com>
2014-11-21 07:00:29 -0600

    -Created XnaLogic.

---
+++ SOLXNA/Entities/XnaLogic.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using SOLXNA.Animations;
+using SpearOfLonginus.Entities;
+
+namespace SOLXNA.Entities
+{
+    /// <summary>
+    /// Adds drawing functions to logics.
+    /// </summary>
+    public class XnaLogic : Logic
+    {
+        /// <summary>
+        /// Loads the content.
+        /// </summary>
+        /// <param name="texturecache">The texturecache.</param>
+        public void LoadContent(TextureCache texturecache)
+        {
+
+        }
+
+        /// <summary>
+        /// Unloads the content.
+        /// </summary>
+        public void UnloadContent()
+        {
+
+        }
+
+        /// <summary>
+        /// Draw before the owner draws its animation.
+        /// </summary>
+        /// <param name="spritebatch">The spritebatch used to draw.</param>
+        public void DrawBefore(SpriteBatch spritebatch)
+        {
+
+        }
+
+        /// <summary>
+        /// Draw after the owner draws its animation.
+        /// </summary>
+        /// <param name="spritebatch">The spritebatch used to draw.</param>
+        public void DrawAfter(SpriteBatch spritebatch)
+        {
+
+        }
+    }
+}
