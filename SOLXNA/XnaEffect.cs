88b6052ee9a82bfb29b4a86a8f15ba8977cae223
Justin <justinhughart@gmail.com>
2014-11-21 06:51:13 -0600

    -Added XnaEffect.

---
+++ SOLXNA/XnaEffect.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using SpearOfLonginus;
+
+namespace SOLXNA
+{
+    /// <summary>
+    /// Adds drawing functions for effects.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class XnaEffect <T> : Effect<T>
+    {
+        /// <summary>
+        /// Draw before the owner draws its animation.
+        /// </summary>
+        /// <param name="spritebatch">The spritebatch used to draw.</param>
+        public void DrawBefore(SpriteBatch spritebatch)
+        {
+
+        }
+
+        /// <summary>
+        /// Draw after the owner draws its animation.
+        /// </summary>
+        /// <param name="spritebatch">The spritebatch used to draw.</param>
+        public void DrawAfter(SpriteBatch spritebatch)
+        {
+
+        }
+    }
+}
