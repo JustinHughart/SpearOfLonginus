413687219ffb0f02effda455eabe61e0862a7373
Justin Hughart <justinhughart@gmail.com>
2014-11-22 04:41:33 -0800

    -Used IXnaDrawable on XnaLogic.

--- SOLXNA/Entities/XnaLogic.cs
+++ SOLXNA/Entities/XnaLogic.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using SOLXNA.Animations;
 using SpearOfLonginus.Entities;
 
@@ -7,8 +8,16 @@ namespace SOLXNA.Entities
     /// <summary>
     /// Adds drawing functions to logics.
     /// </summary>
-    public class XnaLogic : Logic
+    public class XnaLogic : Logic, IXnaDrawable
     {
+        /// <summary>
+        /// Gets or sets the sprite batch data.
+        /// </summary>
+        /// <value>
+        /// The sprite batch data.
+        /// </value>
+        public SpriteBatchData SpriteBatchData { get; set; }
+
         /// <summary>
         /// Loads the content.
         /// </summary>
@@ -27,19 +36,31 @@ namespace SOLXNA.Entities
         }
 
         /// <summary>
-        /// Draw before the owner draws its animation.
+        /// This is not used in the base SOLXNA system. Please use DrawBefore or DrawAfter instead.
         /// </summary>
         /// <param name="spritebatch">The spritebatch used to draw.</param>
-        public void DrawBefore(SpriteBatch spritebatch)
+        /// <param name="cameramatrix">The camera's matrix.</param>
+        public void Draw(SpriteBatch spritebatch, Matrix cameramatrix)
         {
 
         }
 
         /// <summary>
-        /// Draw after the owner draws its animation.
+        /// Draw before the entity draws its animation.
         /// </summary>
         /// <param name="spritebatch">The spritebatch used to draw.</param>
-        public void DrawAfter(SpriteBatch spritebatch)
+        /// <param name="cameramatrix">The camera's matrix.</param>
+        public void DrawBefore(SpriteBatch spritebatch, Matrix cameramatrix)
+        {
+
+        }
+
+        /// <summary>
+        /// Draw after the entity draws its animation.
+        /// </summary>
+        /// <param name="spritebatch">The spritebatch used to draw.</param>
+        /// <param name="cameramatrix">The camera's matrix.</param>
+        public void DrawAfter(SpriteBatch spritebatch, Matrix cameramatrix)
         {
 
         }
