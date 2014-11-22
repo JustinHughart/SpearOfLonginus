e635558d8aa61e0b46898d2a82775b75ef5577eb
Justin Hughart <justinhughart@gmail.com>
2014-11-22 05:09:28 -0800

    -Added GlobalVariables.

---
+++ SpearOfLonginus/GlobalVariables.cs
@@ -0,0 +1,15 @@
+﻿using SpearOfLonginus.UI;
+
+namespace SpearOfLonginus
+{
+    /// <summary>
+    /// A class containing global variables for Spear of Longinus.
+    /// </summary>
+    public static class GlobalVariables
+    {
+        /// <summary>
+        /// The UI Manager
+        /// </summary>
+        public static UIManager UIManager;
+    }
+}
