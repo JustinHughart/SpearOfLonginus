5a091a36008fe802dd573a5ab633e52836efca35
Justin Hughart <justinhughart@gmail.com>
2014-11-22 03:53:24 -0800

    -Added documentation to UIItem.

--- SpearOfLonginus/UI/UIItem.cs
+++ SpearOfLonginus/UI/UIItem.cs
@@ -1,7 +1,13 @@
 ﻿namespace SpearOfLonginus.UI
 {
+    /// <summary>
+    /// An item for a UI.
+    /// </summary>
     public abstract class UIItem
     {
+        /// <summary>
+        /// Updates this instance.
+        /// </summary>
         public virtual void Update()
         {
 
