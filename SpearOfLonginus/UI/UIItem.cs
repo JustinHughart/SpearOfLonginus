e3174aad51eca900c84daa013f77c68f8303a7e4
Justin Hughart <justinhughart@gmail.com>
2014-11-22 04:10:25 -0800

    -Added IXmlLoadable to UIItem.

--- SpearOfLonginus/UI/UIItem.cs
+++ SpearOfLonginus/UI/UIItem.cs
@@ -1,10 +1,17 @@
-﻿namespace SpearOfLonginus.UI
+﻿using System.Xml.Linq;
+
+namespace SpearOfLonginus.UI
 {
     /// <summary>
     /// An item for a UI.
     /// </summary>
-    public abstract class UIItem
+    public abstract class UIItem : IXmlLoadable
     {
+        /// <summary>
+        /// The item's identifier.
+        /// </summary>
+        public string ID;
+
         /// <summary>
         /// Updates this instance.
         /// </summary>
@@ -12,6 +19,15 @@
         public virtual void Update(float deltatime)
         {
 
+        }
+
+        /// <summary>
+        /// Uses XML to initialize the object.
+        /// </summary>
+        /// <param name="element">The element used for loading.</param>
+        public void LoadFromXml(XElement element)
+        {
+            
         }
     }
 }
