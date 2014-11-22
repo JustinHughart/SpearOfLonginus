c62c41e9ad42ad4f6b7af8b3fe991ab5e204b975
Justin Hughart <justinhughart@gmail.com>
2014-11-22 04:23:00 -0800

    -Created IXnaDrawable.

---
+++ SOLXNA/IXnaDrawable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SOLXNA.Animations;
+
+namespace SOLXNA
+{
+    /// <summary>
+    /// Adds drawing functions. 
+    /// </summary>
+    public interface IXnaDrawable
+    {
+        /// <summary>
+        /// Gets or sets the sprite batch data.
+        /// </summary>
+        /// <value>
+        /// The sprite batch data.
+        /// </value>
+        SpriteBatchData SpriteBatchData { get; set; }
+
+        /// <summary>
+        /// Loads the content.
+        /// </summary>
+        /// <param name="texturecache">The texturecache.</param>
+        void LoadContent(TextureCache texturecache);
+
+        /// <summary>
+        /// Unloads the content.
+        /// </summary>
+        void UnloadContent();
+
+        /// <summary>
+        /// Draw what the object needs to.
+        /// </summary>
+        /// <param name="spritebatch">The spritebatch used to draw.</param>
+        /// <param name="cameramatrix">The camera's matrix.</param>
+        void Draw(SpriteBatch spritebatch, Matrix cameramatrix);
+
+        /// <summary>
+        /// Draw before something happens. 
+        /// </summary>
+        /// <param name="spritebatch">The spritebatch used to draw.</param>
+        /// <param name="cameramatrix">The camera's matrix.</param>
+        void DrawBefore(SpriteBatch spritebatch, Matrix cameramatrix);
+
+        /// <summary>
+        /// Draw after something happens.
+        /// </summary>
+        /// <param name="spritebatch">The spritebatch used to draw.</param>
+        /// <param name="cameramatrix">The camera's matrix.</param>
+        void DrawAfter(SpriteBatch spritebatch, Matrix cameramatrix);
+    }
+}
