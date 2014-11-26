20a1d863fb20b6c6fe96ebf513a95ee3313c45e3
Justin <justinhughart@gmail.com>
2014-11-26 14:25:40 -0600

    -Added Textbox.

---
+++ SpearOfLonginus/UI/Data/Textbox.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml.Linq;
+
+namespace SpearOfLonginus.UI.Data
+{
+    /// <summary>
+    /// A scalable text box for Spear of Longinus.
+    /// </summary>
+    public class Textbox : IXmlLoadable
+    {
+        /// <summary>
+        /// The text to be displayed.
+        /// </summary>
+        protected string Text;
+        /// <summary>
+        /// The area of the screen the box takes up.
+        /// </summary>
+        protected Rectangle Area;
+        /// <summary>
+        /// The box's texture's ID.
+        /// </summary>
+        protected string TextureID;
+
+        /// <summary>
+        /// Updates the specified deltatime.
+        /// </summary>
+        /// <param name="deltatime">The deltatime.</param>
+        public void Update(float deltatime)
+        {
+            
+        }
+
+        /// <summary>
+        /// Uses XML to initialize the object.
+        /// </summary>
+        /// <param name="element">The element used for loading.</param>
+        public virtual void LoadFromXml(XElement element)
+        {
+            
+        }
+    }
+}
