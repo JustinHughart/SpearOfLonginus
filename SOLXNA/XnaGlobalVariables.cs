f29c92a1b346a68dfe26cc8bac2f077e94c09923
Justin <justinhughart@gmail.com>
2014-11-26 15:21:43 -0600

    -Added XnaGlobalVariables.

---
+++ SOLXNA/XnaGlobalVariables.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace SOLXNA
+{
+    /// <summary>
+    /// Global variables for SOLXNA.
+    /// </summary>
+    public static class XnaGlobalVariables
+    {
+        /// <summary>
+        /// The default textbox font.
+        /// </summary>
+        public static SpriteFont TextboxFont;
+    }
+}
