aeab0fe9ad5c0732318e28f59982c4f1534792d7
Justin <justinhughart@gmail.com>
2014-11-26 13:56:59 -0600

    -Added DialogUI.

---
+++ SpearOfLonginus/UI/Items/DialogUI.cs
@@ -0,0 +1,26 @@
+﻿namespace SpearOfLonginus.UI.Items
+{
+    public class DialogUI : UIItem
+    {
+        #region Variables
+
+        protected string 
+
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Functions
+
+        public override void LoadFromXml(System.Xml.Linq.XElement element)
+        {
+            
+        } 
+
+        #endregion
+
+    }
+}
