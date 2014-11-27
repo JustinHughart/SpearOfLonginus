55a5078fecc9b719a742c14b3ad34f059ccb1f28
Justin <justinhughart@gmail.com>
2014-11-27 05:10:16 -0600

    -Updated DialogUI.

--- SpearOfLonginus/UI/Items/DialogUI.cs
+++ SpearOfLonginus/UI/Items/DialogUI.cs
@@ -1,9 +1,14 @@
-﻿namespace SpearOfLonginus.UI.Items
+﻿using System.Xml.Linq;
+using SpearOfLonginus.UI.Data;
+
+namespace SpearOfLonginus.UI.Items
 {
     public class DialogUI : UIItem
     {
         #region Variables
 
+        protected Textbox Namebox;
+        protected Textbox Textbox;
         
 
         #endregion
@@ -14,9 +19,26 @@
 
         #region Functions
 
-        public override void LoadFromXml(System.Xml.Linq.XElement element)
+        public override void Update(float deltatime)
+        {
+            if (Textbox != null)
+            {
+                Textbox.Update(deltatime);
+            }
+
+            if (Namebox != null)
+            {
+                Namebox.Update(deltatime);
+            }
+        }
+
+
+        public override void LoadFromXml(XElement element)
         {
             
+
+
+
         } 
 
         #endregion
