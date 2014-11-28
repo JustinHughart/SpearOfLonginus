fadda79268581a8590428b81627aeac2366958a9
Justin Hughart <justinhughart@gmail.com>
2014-11-28 02:37:43 -0800

    -Completed Mainform.

--- SOLCutsceneGenerator/MainForm.Designer.cs
+++ SOLCutsceneGenerator/MainForm.Designer.cs
@@ -28,20 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.menuFileMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnCutscene = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.menuFileMenu.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtID);
+            this.panel1.Controls.Add(this.lblID);
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(258, 38);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(79, 4);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(168, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(49, 7);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(24, 13);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID: ";
+            // 
+            // menuFileMenu
+            // 
+            this.menuFileMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuFileMenu.Location = new System.Drawing.Point(0, 0);
+            this.menuFileMenu.Name = "menuFileMenu";
+            this.menuFileMenu.Size = new System.Drawing.Size(381, 24);
+            this.menuFileMenu.TabIndex = 1;
+            this.menuFileMenu.Text = "FileMenu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.NewToolStripMenuItemClick);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItemClick);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItemClick);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.SaveAsToolStripMenuItemClick);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btnCutscene);
+            this.panel2.Location = new System.Drawing.Point(277, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(92, 38);
+            this.panel2.TabIndex = 8;
+            // 
+            // btnCutscene
+            // 
+            this.btnCutscene.Location = new System.Drawing.Point(8, 6);
+            this.btnCutscene.Name = "btnCutscene";
+            this.btnCutscene.Size = new System.Drawing.Size(75, 23);
+            this.btnCutscene.TabIndex = 0;
+            this.btnCutscene.Text = "Cutscene";
+            this.btnCutscene.UseVisualStyleBackColor = true;
+            this.btnCutscene.Click += new System.EventHandler(this.BtnCutsceneClick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(381, 77);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuFileMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuFileMenu;
             this.Name = "MainForm";
             this.Text = "Spear of Longinus Cutscene Generator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuFileMenu.ResumeLayout(false);
+            this.menuFileMenu.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.MenuStrip menuFileMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnCutscene;
     }
 }
 
--- SOLCutsceneGenerator/MainForm.cs
+++ SOLCutsceneGenerator/MainForm.cs
@@ -1,19 +1,309 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.IO.Compression;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using SOLEntityGenerator;
 
 namespace SOLCutsceneGenerator
 {
+    /// <summary>
+    /// The main form of the too,.
+    /// </summary>
     public partial class MainForm : Form
     {
+        /// <summary>
+        /// The path to the file we're working on.
+        /// </summary>
+        private string _filepath;
+        /// <summary>
+        /// The cutscene element
+        /// </summary>
+        public XElement CutsceneElement;
+       
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainForm"/> class.
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
+            CreateNewCutscene();
+        }
+
+        /// <summary>
+        /// Creates a new cutscene.
+        /// </summary>
+        private void CreateNewCutscene()
+        {
+            _filepath = "";
+            ChangeTitle();
+
+            CutsceneElement = new XElement("next");
+
+            txtID.Text = "";
+        }
+
+        /// <summary>
+        /// Changes the title of the window.
+        /// </summary>
+        private void ChangeTitle()
+        {
+            Text = "SoL Cutscene Generator - " + _filepath;
+
+            if (_filepath.Equals(""))
+            {
+                Text += "New Cutscene";
+            }
+        }
+
+        /// <summary>
+        /// Triggers when the new button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void NewToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            CreateNewCutscene();
+        }
+
+        /// <summary>
+        /// Triggers when the open button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void OpenToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Spear of Longinus Cutscene (*.slc)|*.slc|Spear of Longinus Gzipped Cutscene (*.gzc)|*.gzc";
+            ofd.ShowDialog();
+
+            if (ofd.FileName == "")
+            {
+                return;
+            }
+
+            CreateNewCutscene(); //Initialize the data.
+
+            _filepath = ofd.FileName;
+
+            ChangeTitle();
+
+            LoadFromFile(_filepath);
+        }
+
+        /// <summary>
+        /// Triggers when the save button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void SaveToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            if (_filepath == "")
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Spear of Longinus Cutscene (*.slc)|*.slc|Spear of Longinus Gzipped Cutscene (*.gzc)|*.gzc";
+                sfd.ShowDialog();
+
+                if (sfd.FileName != "")
+                {
+                    _filepath = sfd.FileName;
+
+                    ChangeTitle();
+                }
+            }
+
+            if (_filepath != "")
+            {
+                SaveToFile(_filepath);
+            }
+        }
+
+        /// <summary>
+        /// Triggers when the save as button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void SaveAsToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            bool valid = false;
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Spear of Longinus Cutscene (*.slc)|*.slc|Spear of Longinus Gzipped Cutscene (*.gzc)|*.gzc";
+            sfd.ShowDialog();
+
+            if (sfd.FileName != "")
+            {
+                valid = true;
+                _filepath = sfd.FileName;
+                ChangeTitle();
+            }
+
+            if (valid)
+            {
+                SaveToFile(_filepath);
+            }
+        }
+
+        /// <summary>
+        /// Triggers when the exit button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void ExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+       /// <summary>
+        /// Saves to file.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <exception cref="System.ArgumentException">File type not supported.</exception>
+        private void SaveToFile(string path)
+        {
+            path = ValidatePath(path);
+
+            XElement saveelement = new XElement("cutscene");
+
+            //Basic attributes
+            saveelement.Add(new XAttribute("id", txtID.Text));
+
+            //Custom XML.
+            saveelement.Add(CutsceneElement);
+
+            //First we'll back up the old file.
+            if (File.Exists(path))
+            {
+                string backuppath = path + ".bak";
+
+                if (File.Exists(backuppath))
+                {
+                    File.Delete(backuppath);
+                }
+
+                File.Copy(path, backuppath);
+                File.Delete(path);
+            }
+
+            //Then save it!
+            var doc = new XDocument(saveelement);
+
+            if (path.Contains(".slc"))
+            {
+                using (var stream = File.OpenWrite(path))
+                {
+                    doc.Save(stream);
+                }
+
+                return;
+            }
+
+            if (path.Contains(".gzc"))
+            {
+                using (var stream = File.OpenWrite(path))
+                {
+                    using (var gzipstream = new GZipStream(stream, CompressionMode.Compress))
+                    {
+                        doc.Save(gzipstream);
+                    }
+                }
+
+                return;
+            }
+
+            throw new ArgumentException("File type not supported.");
+        }
+
+        /// <summary>
+        /// Loads from file.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <exception cref="System.ArgumentException">File type not supported.</exception>
+        /// <exception cref="System.Xml.XmlException">No root.
+        /// or
+        /// Root is not an cutscene.</exception>
+        private void LoadFromFile(string path)
+        {
+            path = ValidatePath(path);
+
+            XDocument doc = null;
+
+            //Load the doc.
+
+            if (path.Contains(".slc"))
+            {
+                doc = XDocument.Load(path);
+            }
+            else if (path.Contains(".gzc"))
+            {
+                using (var filestream = File.OpenRead(path))
+                {
+                    using (var gzipstream = new GZipStream(filestream, CompressionMode.Decompress))
+                    {
+                        doc = XDocument.Load(gzipstream);
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException("File type not supported.");
+            }
+
+            //Initial disqualifiers.
+
+            if (doc.Root == null)
+            {
+                throw new XmlException("No root.");
+            }
+
+            if (!doc.Root.Name.LocalName.Equals("cutscene", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new XmlException("Root is not an cutscene.");
+            }
+
+            //First, we'll get the attributes.
+            foreach (var attribute in doc.Root.Attributes())
+            {
+                if (attribute.Name.LocalName.Equals("id", StringComparison.OrdinalIgnoreCase))
+                {
+                    txtID.Text = attribute.Value;
+                    continue;
+                }
+            }
+
+           //Assign the custom xml elements.
+
+            if (doc.Root.Element("next") != null)
+            {
+                CutsceneElement = doc.Root.Element("next");
+            }
+        }
+
+        /// <summary>
+        /// Validates the path.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns></returns>
+        private string ValidatePath(string input)
+        {
+            while (input.EndsWith(" "))
+            {
+                input.Remove(input.Length - 2);
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Triggers when the cutscene button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void BtnCutsceneClick(object sender, EventArgs e)
+        {
+            var form = new ComponentForm(CutsceneElement);
+            form.Show();
         }
     }
 }
